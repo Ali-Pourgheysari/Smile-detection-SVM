abdb5f5b41bf26927a85f7605ec4bfbef6f25549
Ali Pourgheysari <a.geysar@gmail.com>
2023-05-21 21:40:19 +0330

    use model on webcam

--- test_model.py
+++ test_model.py
@@ -1,7 +1,63 @@
 import numpy as np
-from skimage.color import rgb2gray
-from sklearn import svm
+import cv2
 import joblib
-from image_normalize import load_data
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from train_model import hog_lbp
+
+
+def webcam_smile_detectiion(model, model_name = 'svm_model.joblib'):
+    cap=cv2.VideoCapture(0)
+    model = joblib.load(model_name)
+    # fgbg=cv2.createBackgroundSubtractorMOG2()
+
+    while True:
+        # Read frame from video capture
+        ret, frame = cap.read()
+        if ret:
+        # Convert frame to grayscale
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Detect faces using a face detector
+            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            # Loop over all detected faces
+            for (x, y, w, h) in faces:
+                # Extract features from the detected face using a pre-trained deep learning model
+                # and reshape the features to match the input shape of the SVM classifier
+                size = (250, 250)
+                resized_image = cv2.resize(frame[y:y+h, x:x+w], size)
+                hog, lbp = hog_lbp(resized_image)
+
+                features = []
+                features.extend(hog)
+                features.extend(lbp)
+
+                features = np.array(features)
+                features = features.reshape(1,-1)
+                # Use the SVM classifier to predict whether the detected face is smiling or not
+                prediction = model.predict(features)
+
+                # If the SVM predicts a smile, draw a rectangle around the face and display a message
+                if int(*prediction) == 1:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    cv2.putText(frame, 'Smiling', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            # Display the resulting frame
+            cv2.imshow('Smile Detector', frame)
+
+            # Exit loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+
+
+
+def main():
+    model_name = 'svm_model.joblib'
+    model = joblib.load(model_name)
+
+    webcam_smile_detectiion(model)
+
+
+if __name__ == '__main__':
+    main()
--- train_model.py
+++ train_model.py
@@ -1,41 +1,47 @@
-import numpy as np
-from skimage.feature import hog, local_binary_pattern
-from skimage.color import rgb2gray
-from sklearn import svm
 import joblib
-from image_normalize import load_data
-from sklearn.model_selection import train_test_split
+import numpy as np
+from skimage.color import rgb2gray
+from skimage.feature import hog, local_binary_pattern
+from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+from image_normalize import load_data
 
 
-def extreact_features(images):
+def extract_features(images):
 
     # Assuming your images are stored in a list called "images"
     hog_features = []
     lbp_features = []
 
     for image in images:
-        # Convert the image to grayscale
-        gray_image = rgb2gray(image)
-
-        # Extract HOG features
-        hog_feature_vector = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                                 cells_per_block=(1, 1), channel_axis=-1)
+        hog_feature_vector, hist = hog_lbp(image)
         hog_features.append(hog_feature_vector)
-
-        # Extract LBP features
-        lbp_feature_vector = local_binary_pattern(gray_image, 24, 3).ravel()
-
-        # histogram of LBP features
-        hist, _ = np.histogram(lbp_feature_vector, 256, (0, 256))
         lbp_features.append(hist)
 
     # Combine the feature vectors into a single matrix
     features = [[*x, *y] for x, y in zip(hog_features, lbp_features)]
     return features
 
+def hog_lbp(image):
+    # Convert the image to grayscale
+    gray_image = rgb2gray(image)
 
-def train_and_save(X_train, y_train, model_name):
+    # Extract HOG features
+    hog_feature_vector = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                                cells_per_block=(1, 1), channel_axis=-1)
+
+    # Extract LBP features
+    lbp_feature_vector = local_binary_pattern(gray_image, 24, 3).ravel()
+
+    # histogram of LBP features
+    hist, _ = np.histogram(lbp_feature_vector, 256, (0, 256))
+    
+    return hog_feature_vector, hist
+
+
+def train_and_save(X_train, y_train, model_name = 'svm_model.joblib'):
     # Choose an appropriate kernel function
     model = svm.SVC(gamma="auto", kernel='rbf')
 
@@ -48,7 +54,7 @@ def train_and_save(X_train, y_train, model_name):
     joblib.dump(model, model_name)
 
 
-def test(X_test, y_test, model_name):
+def test(X_test, y_test, model_name = 'svm_model.joblib'):
 
     # Load SVM model from disk
     model = joblib.load(model_name)
@@ -71,17 +77,15 @@ def main():
     shuffled_images, shuffled_labels = zip(*combined)
 
     # extract features
-    features = extreact_features(shuffled_images)
+    features = extract_features(shuffled_images)
 
     # split data of train and test
     X_train, X_test, y_train, y_test = train_test_split(
         features, shuffled_labels, test_size=0.25)
 
-    model_name = 'svm_model.joblib'
-
     # train and test the model
-    train_and_save(X_train, y_train, model_name)
-    test(X_test, y_test, model_name)
+    train_and_save(X_train, y_train)
+    test(X_test, y_test)
 
 
 if __name__ == '__main__':
